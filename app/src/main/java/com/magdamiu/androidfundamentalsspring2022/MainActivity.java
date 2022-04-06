88d04992fe2672cf0e90dffc16936e7b76c6cc59
Maria Miu <mmiu@Marias-MacBook-Pro.local>
2022-04-06 19:39:51 +0300

    WebView sample

--- app/src/main/java/com/magdamiu/androidfundamentalsspring2022/MainActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsspring2022/MainActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.views_first_part);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.views_first_part);
 
         String messageToDisplay = "this is a message";
         Log.e("MainActivity", messageToDisplay);
         Log.w("MainActivity", "warning message");
+
+        WebView webViewOneSample = findViewById(R.id.webViewSample);
+        webViewOneSample.getSettings().setJavaScriptEnabled(true);
+        webViewOneSample.loadUrl("https://developer.android.com/");
     }
 }
