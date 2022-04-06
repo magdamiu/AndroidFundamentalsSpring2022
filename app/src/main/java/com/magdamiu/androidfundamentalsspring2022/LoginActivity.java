3c2e2e1984adc1cfd9c5408de3ea76d31267e200
Maria Miu <mmiu@Marias-MacBook-Pro.local>
2022-04-06 21:06:53 +0300

    Sample with Login screen (happy path - there are not covered all the unhappy cases)

---
+++ app/src/main/java/com/magdamiu/androidfundamentalsspring2022/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.magdamiu.androidfundamentalsspring2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText editTextEmail, editTextPassword;
+    private TextView textViewDisplayContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        textViewDisplayContent = findViewById(R.id.displayContentTextView);
+    }
+
+    public void loginOnClick(View view) {
+        // step 1: take the strings from the EditTexts
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        // step 1.1: check if email address is filled
+        if (email.isEmpty()) {
+            editTextEmail.setError("Please add an email address");
+
+            // step 1.2: check if email address is valid
+            // Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        } else {
+            // step 2: display the strings from EditTexts inside of the TextView
+            textViewDisplayContent.setText(email + " " + password);
+        }
+    }
+}
