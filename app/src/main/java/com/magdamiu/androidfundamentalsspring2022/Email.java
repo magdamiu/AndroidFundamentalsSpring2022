252846602fb47a199f7c745302d35689f258216b
Maria Miu <mmiu@Marias-MacBook-Pro.local>
2022-04-09 15:40:17 +0300

    Step 1: get data source for RecyclerView

---
+++ app/src/main/java/com/magdamiu/androidfundamentalsspring2022/Email.java
@@ -0,0 +1,37 @@
+package com.magdamiu.androidfundamentalsspring2022;
+
+public class Email {
+    private String subject;
+    private String details;
+    private String sender;
+
+    public Email(String subject, String details, String sender) {
+        this.subject = subject;
+        this.details = details;
+        this.sender = sender;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
---
+++ app/src/main/java/com/magdamiu/androidfundamentalsspring2022/EmailsListActivity.java
@@ -0,0 +1,50 @@
+package com.magdamiu.androidfundamentalsspring2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmailsListActivity extends AppCompatActivity {
+
+    private List<Email> emails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emails_list);
+    }
+
+    // step 1: get data source
+    private void getEmails() {
+        emails = new ArrayList<>();
+        Email email1 = new Email("Hello from Pitesti", "Have a nice day", "Emilia");
+        Email email2 = new Email("New project", "New project for your team", "John");
+        Email email3 = new Email("Status updates", "These are the updates from March", "Julie");
+        Email email4 = new Email("Course 1 - Android", "Find the attached documents", "Ioana");
+        Email email5 = new Email("Lab 1 - Android", "Check the code changes", "Alex");
+        Email email6 = new Email("Lab 2 - Android", "Check the code changes", "Cosmin");
+        Email email7 = new Email("Lab 3 - Android", "Check the code changes", "Maria");
+        Email email8 = new Email("Lab 4 - Android", "Check the code changes", "Denisa");
+        Email email9 = new Email("Lab 5 - Android", "Check the code changes", "Anca");
+        Email email10 = new Email("Lab 6 - Android", "Check the code changes", "Andreea");
+
+        emails.add(email1);
+        emails.add(email2);
+        emails.add(email3);
+        emails.add(email4);
+        emails.add(email5);
+        emails.add(email6);
+        emails.add(email7);
+        emails.add(email8);
+        emails.add(email9);
+        emails.add(email10);
+    }
+
+
+    // step 2: get adapter
+
+    // step 3: map the adapter to the RecyclerView
+}
