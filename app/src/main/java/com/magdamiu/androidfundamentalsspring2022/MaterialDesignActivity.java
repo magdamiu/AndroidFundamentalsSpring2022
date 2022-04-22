e769f04ad27ae9669f84167d61d3ce875279f8f0
Maria Miu <mmiu@Marias-MacBook-Pro.local>
2022-04-22 11:01:13 +0300

    Animation with gradients

--- app/src/main/java/com/magdamiu/androidfundamentalsspring2022/MaterialDesignActivity.java
+++ app/src/main/java/com/magdamiu/androidfundamentalsspring2022/MaterialDesignActivity.java
@@ -1,6 +1,8 @@
 package com.magdamiu.androidfundamentalsspring2022;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,15 @@ public class MaterialDesignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material_design);
+
+        setAnimation();
+    }
+
+    private void setAnimation() {
+        LinearLayout linearLayout = findViewById(R.id.linearLayoutMaterialDesign);
+        AnimationDrawable animationDrawable = (AnimationDrawable) linearLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
     }
 }
